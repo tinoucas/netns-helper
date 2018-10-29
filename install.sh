9dac67e169f48081a36bdeb211cfa8eb2afe563a
Etienne Faure <etienne.faure@mixvibes.com>
2018-10-29 08:15:45 +0100

    Fixed: .service files permissions after install.

--- install.sh
+++ install.sh
@@ -4,7 +4,13 @@ PREFIX="$(echo "$1" | sed 's#^\(.*\)/$#\1#')"
 
 find usr etc -type f | while read FILE
 do
-	install -v -D $FILE -t $PREFIX/$(dirname $FILE)
+	MODE=
+	if [ -n "$(echo $FILE | grep .service)" ]; then
+		MODE=0644
+	else
+		MODE=0755
+	fi
+	install -m $MODE -v -D $FILE -t $PREFIX/$(dirname $FILE)
 done
 
 systemctl daemon-reload
